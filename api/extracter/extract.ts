8c9bfff3505fd0d7c38196f437f98cd2b4cf76a4
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2021-09-04 23:05:35 +0200

    feat: add final generator

--- api/extracter/extract.ts
+++ api/extracter/extract.ts
@@ -1,3 +1,16 @@
 import { loadFromDisk } from './raw-db/raw-fs'
+import { validate } from './raw-db/raw-validator'
+import { remapper } from './raw-to-final/final-extracter'
+import { writeToDisk } from './raw-to-final/final-fs'
 
-loadFromDisk()
+async function main() {
+  const rawDb = await loadFromDisk('yaml')
+
+  if (!validate(rawDb)) {
+    process.exit(-1)
+  }
+
+  writeToDisk(remapper(rawDb))
+}
+
+main()
