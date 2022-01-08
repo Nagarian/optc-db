b92794fbed74598ae3c623ad43b09fbb4cc30fe2
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2022-01-08 15:25:31 +0100

    chore: remove fest* properties from old-db

--- api/extracter/old-to-raw/fixup/specific-issue.ts
+++ api/extracter/old-to-raw/fixup/specific-issue.ts
@@ -6,7 +6,6 @@ export function fixupSpecificIssue(
   _units: OldDB.ExtendedUnit[],
 ): OldDB.ExtendedUnit {
   fixWrongPotential(unit)
-  fixFestResilience(unit)
   return unit
 }
 
@@ -20,15 +19,6 @@ function fixWrongPotential(unit: OldDB.ExtendedUnit) {
     unit.detail.limit[14].description = `Acquire Sailor Ability 2: ${unit.detail.sailor.level1}`
   } else {
     console.warn('issue with unit 1538 has been fixed')
-  }
-}
-
-function fixFestResilience(unit: OldDB.ExtendedUnit) {
-  const detail = unit.detail as any
-
-  if (detail.festResilience) {
-    detail.festResistance = detail.festResilience
-    delete detail.festResilience
   }
 }
 
