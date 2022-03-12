215f697cf4a888538100c4a37f63b3e9b2cabd59
Benoît Garreau <onyx_nagarian47@hotmail.fr>
2022-03-12 11:23:38 +0100

    fix: dual unit remapping

--- api/extracter/old-to-raw/fixup/dual-versus.ts
+++ api/extracter/old-to-raw/fixup/dual-versus.ts
@@ -14,26 +14,18 @@ export function fixupDualVersusMapping(
     return unit
   }
 
-  const name = unit.name.substring(unit.name.indexOf(',') + 1).trim()
+  const name = unit.name.substring(unit.name.indexOf(' - ') + 3).trim()
   if (name === unit.name && !complexMapping[unit.dbId]) {
     throw new Error(
-      `unit ${unit.dbId} "${unit.name}" is a dual unit without a comma in his name`,
+      `unit ${unit.dbId} "${unit.name}" is a dual unit without a dash in his name`,
     )
   }
 
-  let duals = units
-    .filter(isGhostUnit)
-    .filter(
-      u =>
-        u.name.includes(name) ||
-        complexMapping[unit.dbId]?.some(str => u.name.includes(str)),
-    )
-
-  if (complexMapping[unit.dbId]) {
-    duals = units
-      .filter(isGhostUnit)
-      .filter(u => complexMapping[unit.dbId].includes(u.name))
-  }
+  let duals = complexMapping[unit.dbId]
+    ? units
+        .filter(isGhostUnit)
+        .filter(u => complexMapping[unit.dbId].includes(u.name))
+    : units.filter(isGhostUnit).filter(u => u.name.includes(name))
 
   const isValidVersus = isVersusClass(unit.class) && duals.length === 2
   const isValidDualMonoType =
@@ -44,7 +36,7 @@ export function fixupDualVersusMapping(
 
   if (!isValidVersus && !isValidDualMonoType && !isValidDual) {
     throw new Error(
-      `unit ${unit.dbId} "${unit.name}" has submapping issue (${
+      `unit ${unit.dbId} "${unit.name}" has submapping issue with "${name}" (${
         duals.length
       } units):\n${duals.map(u => u.name).join('\n')}`,
     )
@@ -61,31 +53,31 @@ export function isGhostUnit(unit: OldDB.ExtendedUnit): boolean {
 }
 
 const complexMapping: Record<number, string[]> = {
+  2000: [
+    '[Dual Unit] Sanji - Peerless Straw Hat Pirates',
+    '[Dual Unit] Zoro - Peerless Straw Hat Pirates',
+    '[Dual Unit] Sanji & Zoro - Peerless Straw Hat Pirates',
+    '[Dual Unit] Sanji & Zoro - Peerless Straw Hat Pirates',
+  ],
   2399: [
-    '[Dual Unit] Crocodile, Revived Pirate',
-    '[Dual Unit] Daz, Revived Assassin',
-    '[Dual Unit] Crocodile & Daz, Revived Duo',
+    '[Dual Unit] Crocodile - Revived Pirate',
+    '[Dual Unit] Daz - Revived Assassin',
+    '[Dual Unit] Crocodile & Daz - Revived Duo',
   ],
   2551: [
     '[Dual Unit] Admiral Sengoku',
     '[Dual Unit] Vice Admiral Garp',
     '[Dual Unit] Admiral Sengoku and Vice Admiral Garp',
   ],
-  2556: [
-    '[Dual Unit] Red Hair Shanks',
-    '[Dual Unit] Ben Beckman',
-    '[Dual Unit] Red Hair Shanks & Ben Beckman',
-  ],
-  3279: ['[Dual Unit] Marshall D. Teach, Two Overwhelming Abilities'],
-  3280: ['[Dual Unit] Blackbeard, Two Overwhelming Abilities'],
+  3279: ['[Dual Unit] Marshall D. Teach - Overwhelming Two Powers'],
+  3280: ['[Dual Unit] Blackbeard - Overwhelming Two Powers'],
   3432: [
     '[Dual Unit] Ulti',
     '[Dual Unit] Page One',
     '[Dual Unit] Ulti & Page One',
   ],
-  3508: [
-    '[Dual Unit] Kaido, Strongest Force in the World',
-    '[Dual Unit] Lead Performers, Strongest Force in the World',
-    '[Dual Unit] Kaido & Lead Performers, Strongest Force in the World'
-  ]
+  3252: [
+    "[VS Unit] Ace - Flame and Magma",
+    "[VS Unit] Akainu - Flame and Magma",
+  ],
 }
